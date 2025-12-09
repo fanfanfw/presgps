85eb8f6da254fdd3914cf2ad17ec912b0da68d81
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-12-09 11:28:58 +0700

    update ngrok trusted

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,8 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    // Trust all proxies so forwarded headers from ngrok/other tunnels are honored
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
